a90950d236f27061236bcb1ff19813756b7fb245
Jhonatan <larico1599@gmail.com>
2022-11-08 13:39:51 -0500

    end Lesson 4.4

--- Project-4/Assets/Scripts/PlayerController.cs
+++ Project-4/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+   
     private Rigidbody playerRb;
     private GameObject focalPoint;
+
+    public GameObject powerupIndicator;
+
+    public float speed = 5.0f;
+    public bool hasPowerup = false;
+    public float powerupStrength = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,38 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {   
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - gameObject.transform.position;
+
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
     }
 }
--- Project-4/Assets/Scripts/SpawnManager.cs
+++ Project-4/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {   
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spwanRange =9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //Vector3 randomPos = GenerateSpawPosition();
 
-        Instantiate(enemyPrefab,GenerateSpawPosition(), enemyPrefab.transform.rotation );
+        SpawnEnemies(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawPosition(), powerupPrefab.transform.rotation);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if(enemyCount == 0) {
+            waveNumber++;
+            SpawnEnemies(waveNumber); 
+            Instantiate(powerupPrefab,GenerateSpawPosition(),powerupPrefab.transform.rotation);
+        }
     }
+    void SpawnEnemies(int enemisToSpawn)
+    {
+        for(int i = 0; i < enemisToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawPosition(), enemyPrefab.transform.rotation);
 
+        }
+    }
     private Vector3 GenerateSpawPosition()
     {
         float spawnPosX = Random.Range(-spwanRange, spwanRange);
